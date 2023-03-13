9457c51fdb6531d59dfc013f413c1f6b9e00f159
Jerry <jerryfu_mc@outlook.com>
2023-03-13 18:13:30 -0400

    update

--- routes/ping.js
+++ routes/ping.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // ping
-router.get('/', function (req, res, next) {
-    res.send('pong!');
+router.get("/", function (req, res, next) {
+    res.send("pong!");
 });
 
 module.exports = router;
